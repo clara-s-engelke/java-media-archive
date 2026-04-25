5357056ac9bee4d1c4389916c8eae93f08fb0cbc
BluHatCat <c1ara.en9e1ke@gmail.com>
2026-04-25 10:45:42 +0200

    Remove .idea from repo

---
+++ src/archive/Util.java
@@ -0,0 +1,23 @@
+package archive;
+
+import items.Item;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Search {
+    public static List<Item> linSearch(List<Item> items, String targetTitle) {
+        List<Item> hits = new ArrayList<>();
+        for (Item item : items) {
+            if (item.getTitle().contains(targetTitle)) {
+                hits.add(item);
+            }
+        }
+        return hits;
+    }
+
+    public static void sort(List<Item> items){
+        Comparator<Item> BY_TITLE = Comparator.comparing()
+    }
+}
